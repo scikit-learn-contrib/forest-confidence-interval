Fixed bug that was averaging trees over the prediction axes. Now the average of the trees is obtained for each prediction and not the average over all predictions of each tree.

forestci/forestci.py
@@ -236,7 +236,7 @@ def random_forest_error(forest, X_train, X_test, inbag=None,
         inbag = calc_inbag(X_train.shape[0], forest)
 
     pred = np.array([tree.predict(X_test) for tree in forest]).T
-    pred_mean = np.mean(pred, 0)
+    pred_mean = np.mean(pred, 1).reshape(X_test.shape[0], 1)
     pred_centered = pred - pred_mean
     n_trees = forest.n_estimators
     V_IJ = _core_computation(X_train, X_test, inbag, pred_centered, n_trees,